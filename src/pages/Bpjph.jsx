53e73d2baf7ae76db9cda6ef7aadd83b9c5b68f6
vatih2675 <ruslansamuel11@gmail.com>
2025-04-10 23:47:27 +0800

    edit data

--- src/pages/Bpjph.jsx
+++ src/pages/Bpjph.jsx
@@ -1,15 +1,14 @@
-import { dataBpjph } from '../constants/Data';
-import BpjphDetail from './BpjphDetail';
+import { dataBpjph } from "../constants/data";
+import BpjphDetail from "./BpjphDetail";
 
 const Bpjph = () => {
   return (
     <section className="w-full p-24 text-gray-700 flex flex-wrap justify-evenly items-center gap-10">
       {dataBpjph.map((bpj, index) => (
         <BpjphDetail key={index} bpj={bpj} />
-        
       ))}
     </section>
   );
-}
+};
 
-export default Bpjph
+export default Bpjph;
